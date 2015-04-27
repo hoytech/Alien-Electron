d0782749328f65c00474cbeeb099ceac499d9917
doug <doug@hcsw.org>
2015-04-26 21:49:14 -0400

    docs

--- lib/Alien/Electron.pm
+++ lib/Alien/Electron.pm
@@ -1,6 +1,6 @@
 package Alien::Electron;
 
-our $VERSION = '0.101';
+our $VERSION = '0.100';
 
 use File::ShareDir;
 
@@ -24,14 +24,18 @@ Alien::Electron - Download and install the electron GUI framework
 
 =head1 DESCRIPTION
 
-This package will download a zip file containing the L<Electron GUI framework|https://github.com/atom/electron> and will then install it into its private distribution share directory.
+This package will download a zip file containing github's L<Electron GUI framework|https://github.com/atom/electron> and will then install it into its private distribution share directory.
 
 The location of the binary is stored in the C<$Alien::Electron::electron_binary> variable:
 
     $ perl -MAlien::Electron -E 'say $Alien::Electron::electron_binary'
     /usr/local/share/perl/5.18.2/auto/share/dist/Alien-Electron/electron
 
-Note however that you probably want to use the L<Valence> module instead of accessing the binary directly.
+Note that you may want to use the L<Valence> module instead of accessing the C<electron> binary directly.
+
+=head1 BUGS
+
+Need to get windows support working.
 
 =head1 SEE ALSO
 
@@ -41,6 +45,8 @@ L<Valence> perl interface
 
 L<Official Electron website|https://github.com/atom/electron>
 
+L<Alien::Thrust>
+
 =head1 AUTHOR
 
 Doug Hoyte, C<< <doug@hcsw.org> >>
@@ -49,12 +55,10 @@ Justin Pacheco
 
 =head1 COPYRIGHT & LICENSE
 
-Copyright 2014 Doug Hoyte.
+Copyright 2015 Doug Hoyte.
 
 This module is licensed under the same terms as perl itself.
 
-This perl distribution downloads compiled binaries of the Electron project which is copyright copyright (c) 2014 GitHub Inc. and is licensed under the MIT license.
-
-
+This perl distribution downloads compiled binaries of the Electron project which is copyright (c) 2014 GitHub Inc. and is licensed under the MIT license.
 
 =cut
